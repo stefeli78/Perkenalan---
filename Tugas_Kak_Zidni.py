7e145641e1fc8fe8968cdd750bef7e32a09405ba
Stefani Felisia Irawan <94348471+stefeli78@users.noreply.github.com>
2021-11-16 13:04:45 +0700

    Add files via upload

---
+++ Tugas_Kak_Zidni.py
@@ -0,0 +1,10 @@
+nama = "Stefani Felisia Irawan"
+nim = "212410102012"
+prodi = "Teknologi Informasi"
+fakul = "Fakultas Ilmu Komputer"
+univ = "Universitas Jember"
+asal = "Jember"
+
+print('=== HHAAIIIIII !!! ===''\n''=== HHAAIIIIII !!! ===')
+print('Perkenalkan nama saya','"',nama,'"','\n' 'Dengan NIM : ', nim, '\n' 'Saya dari prodi', prodi,',', fakul,',',univ,'\n' 'Asal saya dari', asal)
+print('Jangan sungkan-sungkan kenalan yaw','\n' 'Salam Kenal, Pleased to meet you :D')
